b243de4cf20d0fae0a82269727c9533feaf996d0
gliese-backup <glieseb+1@proton.me>
2024-11-04 14:15:31 +0000

    mobile view

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      pen: ["Inter", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
